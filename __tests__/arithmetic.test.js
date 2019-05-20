b6a6b9a07031dfdffc73fdb0b1ac4f78f202ec84
Chloie Parsons <chloieparsons@gmail.com>
2019-05-20 16:18:47 -0700

    Arithmetic tests Passed

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -2,3 +2,26 @@
 
 const arithmetic = require('../lib/arithmetic.js');
 
+test('should return sum of two numbers', () => {
+  let actual = arithmetic.add(1, 2),
+    expectedValue = 3;
+  expect(actual).toBe(expectedValue);
+});
+
+test('should return difference of two numbers', () => {
+  let actual = arithmetic.subtract(2, 1), 
+    expectedValue = 1;
+  expect(actual).toBe(expectedValue);
+});
+
+test('should return product of two numbers', () => {
+  let actual = arithmetic.multiply(2, 1), 
+    expectedValue = 2;
+  expect(actual).toBe(expectedValue);
+});
+
+test('should return quotient of two numbers', () => {
+  let actual = arithmetic.divide(10, 5), 
+    expectedValue = 2;
+  expect(actual).toBe(expectedValue);
+});
